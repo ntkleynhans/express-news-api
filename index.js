28db6e7b3ecc36c5dd34dff5032b17bc10b0773f
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-25 15:42:04 +0200

    basic ping/pong on express server and adding stories json

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
