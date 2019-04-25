12d18fe36b9d050087912c594b8982deb6572626
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-25 16:05:34 +0200

    api route for stories and nodemon

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const stories = require('./stories');
 
 const app = express();
 const PORT = 3000;
@@ -6,6 +7,23 @@ const PORT = 3000;
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
+
+app.get('/stories', (req, res) => {
+  res.json(stories);
+});
+
+/*app.get('/stories/:title', (req, res) => {
+  const { title } = req.params;
+
+  res.json(stories.filter(story => story.title.includes(title)));
+});*/
+
+app.get('/stories/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json(stories.filter(story => story.id == id));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
